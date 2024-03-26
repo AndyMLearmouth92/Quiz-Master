a2fa709cc9351222fce6714b4d27c3e89976b552
Andy Learmouth <andymlearmouth@hotmail.com>
2024-03-26 18:41:39 +0000

    Added the background image for the application.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className="card w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: "url(images/quizBackground.png)" }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content min-h-full min-w-full">
+          <div className="max-w-xl"></div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
