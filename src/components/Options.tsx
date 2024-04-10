9a2021e987b4d513db8e471e43eb394790cb213b
Andy Learmouth <andymlearmouth@hotmail.com>
2024-04-10 18:48:06 +0100

    Changed the size of the buttons to 10/12.

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -12,7 +12,7 @@ export default function Options({ answerOption }: OptionsProps) {
   // const hasAnswered = answer !== null;
   return (
     <button
-      className="btn btn-neutral btn-primary m-2 btn-answer w-11/12 h-14 text-xl"
+      className="btn btn-neutral btn-primary m-2 btn-answer w-10/12 h-14 text-xl"
       onClick={() =>
         dispatch({ type: "newAnswer", payload: answerOption.isCorrect })
       }
