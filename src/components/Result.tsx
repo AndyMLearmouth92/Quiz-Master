9dd350d1b8c016936428e0bb69bcdc03a9ecb718
Andy Learmouth <andymlearmouth@hotmail.com>
2024-04-20 15:45:23 +0100

    Added a high score feature which checks local storage to see if there is a previous high score. This lets the user see how their performance compared to previous performances.

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,37 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuiz } from "../contexts/QuizContext";
 
 // Shows the user their result from the quiz and gives them the option of reviewing their answers or restarting.
 const Result: React.FC = () => {
+  const [message, setMessage] = useState("");
   const { points, numQuestions, dispatch, userAnswers } = useQuiz();
   // Calculates the outcome of the quiz and provides the user with an emoji
   const percentage = (points / numQuestions) * 100;
   let emoji;
   if (percentage === 100) emoji = "🥇";
   if (percentage >= 80 && percentage < 100) emoji = "🎉";
-  if (percentage >= 50 && percentage < 80) emoji = "🙃";
+  if (percentage >= 50 && percentage < 80) emoji = "😎";
   if (percentage >= 0 && percentage < 50) emoji = "🤨";
   if (percentage === 0) emoji = "🤦‍♂️";
 
-  let message = "";
-  // TODO: make this a use ref
+  //Handles the high score functionality using local storage.
   useEffect(() => {
-    const highscore = localStorage.getItem("highscore");
-    if (points > Number(highscore || 0)) {
-      localStorage.setItem("highscore", `${points}`);
-      message = "Well done, you got a new high score!";
+    const highscore = Number(localStorage.getItem("QuizMasterHighScore"));
+    if (percentage > highscore) {
+      localStorage.setItem("QuizMasterHighScore", `${percentage}`);
+      setMessage(`Well done, you have a new high score of ${percentage}%!`);
     } else {
-      message = `Your high score is ${highscore}`;
+      setMessage(
+        `Not a new high score on this occasion, your high score remains at ${highscore}%`
+      );
     }
-  }, [points]);
+  }, [percentage]);
+
   return (
     <div className="card lg:card-side bg-base-100 shadow-xl h-5/6 bg-slate-600 size-3/5">
       <div className="card-body flex">
-        <h1 className="text-5xl mb-8">
+        <h1 className="text-5xl mb-6">
           <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
           {numQuestions} ({Math.ceil(percentage)}%)
         </h1>
-        <h2>{message}</h2>
         <div>
           <div>
             {userAnswers.map((_, i) => {
@@ -55,6 +57,7 @@ const Result: React.FC = () => {
               );
             })}
           </div>
+          <h2 className="text-3xl m-6">{message}</h2>
           <button
             className="btn btn-neutral btn-primary m-2 btn-answer w-11/12 h-14 text-xl"
             onClick={() => dispatch({ type: "reviewAnswers" })}
