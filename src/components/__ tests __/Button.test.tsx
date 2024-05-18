c6fec00cd08c402bb7475e69c496f0675906baae
Andy Learmouth <andymlearmouth@hotmail.com>
2024-05-18 09:34:41 +0100

    Added a test to the button component to test functionality.

--- src/components/__ tests __/Button.test.tsx
+++ src/components/__ tests __/Button.test.tsx
@@ -1,5 +1,5 @@
 import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Button from "../Button";
 
 describe("Button component", () => {
@@ -7,5 +7,16 @@ describe("Button component", () => {
     render(<Button className={""}>Click me</Button>);
     const buttonComponent = screen.getByTestId("buttonComponent");
     expect(buttonComponent).toBeInTheDocument();
+  });
+  test("Tests click functionality", () => {
+    const onClickMock = jest.fn();
+    render(
+      <Button className={""} onClick={onClickMock}>
+        Click me
+      </Button>
+    );
+    const buttonComponent = screen.getByTestId("buttonComponent");
+    fireEvent.click(buttonComponent);
+    expect(onClickMock).toHaveBeenCalledTimes(1);
   });
 });
